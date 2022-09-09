dfbd55bef924fdeacc9c0d0c892d5d22289372db
abhi02-pro <abhinav.nitgoa.2019@gmail.com>
2022-09-10 03:02:38 +0530

    header in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import "./css/Header.css"
 
 function Header() {
     const navlinks = [
-        { name: "Home", url: "/home" },
-        { name: "Register Complaint", url: "/form" },
-        { name: "Complaint Status", url: "/status" },
-        { name: "About", url: "/about" },
+        { name: "Home", url: "/home", class: "home"},
+        { name: "Register Complaint", url: "/form", class: "form"},
+        { name: "Complaint Status", url: "/status", class: "status"},
+        { name: "About", url: "/about", class: "about"},
     ];
     return (
         <div>
             <header>
-                <div className="px-3 py-2 text-bg-dark">
+                <nav>            
+                    <div class="nav-links">
+                        <a class="active home" href="#home">Home</a>
+                        <a class="about" href="#about">About</a>
+                        <a class="experience" href="#experience">Experience</a>
+                        <a class="skills" href="#skills">Skills</a>
+                        <a class="work" href="#work">Work</a>
+                        <a class="connect" href="#connect">Connect</a>
+                    </div>
+                    <div class="burger">
+                        <div class="line1"></div>
+                        <div class="line2"></div>
+                        <div class="line3"></div>
+                    </div>
+                </nav>
+                {/* <div className="px-3 py-2 text-bg-dark">
                     <div className="container">
                         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                             <a
@@ -35,7 +51,7 @@ function Header() {
                             </ul>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </header>
         </div>
     );
