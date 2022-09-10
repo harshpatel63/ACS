a9b32559b6c60d5a08db4f482d37f3c514c4d3b9
thisisbipin <bipinmjadav087@gmail.com>
2022-09-10 16:58:01 +0530

    updated status

--- src/components/Complaints/Status.jsx
+++ src/components/Complaints/Status.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./css/Status.css";
 
 function Status() {
-    return <div>Status</div>;
+    let [complaintID, setcomplaintID] = useState("");
+    function fetchComplaintStatus() {}
+    return (
+        <section className="section-full">
+            {SearchComplaint(setcomplaintID)}
+            <div className="fetch container-center">hi</div>
+        </section>
+    );
 }
 
+function SearchComplaint(_setcomplaintID) {
+    return (
+        <div class="wrap container-center">
+            <div class="search">
+                <input
+                    type="text"
+                    class="searchTerm"
+                    placeholder="Enter Complaint ID"
+                    style={{ height: "100%" }}
+                    onChange={(evt) => {
+                        _setcomplaintID(evt.target.value);
+                    }}
+                />
+                <button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+    );
+}
 export default Status;
