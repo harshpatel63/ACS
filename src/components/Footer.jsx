4f83519a07d3c140c021010de56771fa865d61d8
thisisbipin <bipinmjadav087@gmail.com>
2022-09-10 17:06:02 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./css/Footer.css";
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <div className="footer__row row">
+                    <div className="col-sm-3 about-div">
+                        <h4 className="title">ACS</h4>
+                        <p>
+                            Anonymous Complaint System, pull is always the right
+                            approach but one exception could be when you are
+                            trying to convert.
+                        </p>
+                    </div>
+
+                    <div className="col-sm-3 payment-div">
+                        <h4 className="title">Integrated with</h4>
+                        <ul className="payment">
+                            <li>
+                                <a href="https://metamask.io/" target="_blank">
+                                    <div className="footer__buy_with_meta_mask">
+                                        <img
+                                            src="https://raw.githubusercontent.com/harshpatel63/Decentralized-Rating-System/master/src/assets/Metamask-logo.jpg"
+                                            alt=""
+                                        />
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+
+                <div className="row text-center"> © 2022. Meta Health</div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
