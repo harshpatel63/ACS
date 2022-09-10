2ada23aa3d0cd6d08c8069f923496447fd52e580
Harsh Patel <76950584+harshpatel63@users.noreply.github.com>
2022-09-10 16:36:21 +0530

    Added contract

---
+++ contract/complaint.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.8.17;
+
+contract ComplaintContract {
+
+    uint complaintCount = 0;
+    
+    mapping(uint => Complaint) ComplaintMap;
+
+    struct Complaint {
+        uint id;
+        string name;
+        string title;
+        string location;
+        string dateOfIncident;
+        uint timeOfReporting;
+        uint8 category;
+        string description;
+        string[] imageHash;
+    }
+
+    function getComplaintsList() public view returns (Complaint[] memory) {
+        Complaint[] memory ret = new Complaint[](complaintCount+1);
+        for (uint i = 0; i <= complaintCount; i++) {
+            ret[i] = ComplaintMap[i];
+        }
+        return ret;
+    }
+
+    function createComplaint(string memory name, string memory title, string memory location, string memory dateOfIncident, uint8 category, string memory description, string[] memory imageHash) public {
+        complaintCount++;
+        ComplaintMap[complaintCount].name = name;
+        ComplaintMap[complaintCount].title = title;
+        ComplaintMap[complaintCount].location = location;
+        ComplaintMap[complaintCount].dateOfIncident = dateOfIncident;
+        ComplaintMap[complaintCount].timeOfReporting = block.timestamp;
+        ComplaintMap[complaintCount].category = category;
+        ComplaintMap[complaintCount].description = description;
+        ComplaintMap[complaintCount].imageHash = imageHash;
+    }
+
+}
