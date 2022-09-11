de1050bfd00fcd20a82ee4ac9f769e0a216d881a
abhi02-pro <abhinav.nitgoa.2019@gmail.com>
2022-09-11 19:14:37 +0530

    dashboard charts

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,165 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./css/Dashboard.css"
+// import Chart from 'chart.js'
 
 function Dashboard() {
+  useEffect(()=>{
+    var ctx1 = document.getElementById("myChart1");
+    var ctx2 = document.getElementById("myChart2");
+
+    var myChart1 = new window.Chart(ctx1, {
+      type: 'bar',
+      data: {
+        labels: ['Robbery', 'Others', 'Drugs', 'Murder', 'Rape', 'Kidnapping'],
+        datasets: [{
+          
+          data: [328,281,155,137,44,30],
+          backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Number of Cases by Case Category',
+                font:{
+                  size:20
+                }
+            },    
+            legend: { display: false }       
+        } 
+      }
+    });
+ 
+    var myChart2 = new window.Chart(ctx2, {
+      type: 'bar',
+      data: {
+        labels: ['Verna',
+          'Farmagudi',
+          'Aldona',
+          'Nerul',
+          'Curtorim',
+          'St. Cruz',
+          'Assagao',
+          'Anjuna',
+          'Bicolim',
+          'Arpora',
+          'Ponda city',
+          'Colva',
+          'Satari',
+'          Mopa',
+          'Korgao',
+          'Mapusa',
+          'Tudal',
+          'Shiroda',
+          'Canacona',
+          'Pernem',
+          'Curchorem',
+          'Chandel',
+          'Bambolim',
+          'Vasco da gama',
+          'Quepem',
+          'Divar Island',
+          'Morjim',
+          'Sankhali',
+          'Valpoi',
+          'Madkai',],
+        datasets: [{
+          
+          data: [46,
+            45,
+            44,
+            42,
+            42,
+            39,
+            38,
+            37,
+            37,
+            35,
+            35,
+            34,
+            32,
+            31,
+            31,
+            31,
+            31,
+            30,
+            30,
+            29,
+            29,
+            27,
+            27,
+            27,
+            26,
+            25,
+            24,
+            24,
+            24,
+            23],
+          backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        indexAxis:'y',
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Number of Cases by City',
+                font:{
+                  size:20
+                }
+            },
+            datalabels: { // This code is used to display data values
+                anchor: 'end',
+                align: 'top',
+                formatter: Math.round,
+                font: {
+                    weight: 'bold',
+                    size: 16
+                }
+            },
+            legend: { display: false } 
+        } 
+      }
+    });
+
+  },[])
   return (
     <div className='dashboard-parent-container'>
         <h1>Goa State Crime Report</h1>
@@ -130,7 +288,15 @@ function Dashboard() {
             <hr />
           </div>
         </div>
-            
+        <div className="chart-section">
+          <div className="chart">
+            <canvas id="myChart1" width="400" height="400"></canvas>
+          </div>
+          <br />
+          <div className="chart">
+            <canvas id="myChart2" width="400" height="400"></canvas>
+          </div>
+        </div>
 
     </div>
   )
