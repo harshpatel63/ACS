826a8b8a5778f2c974485c1c891a0d0ac3835c5a
Harsh <patelharsh0603@gmail.com>
2022-09-11 23:49:34 +0530

    Updated link for android app

--- Android/app/src/main/java/com/example/acs/MainActivity.kt
+++ Android/app/src/main/java/com/example/acs/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.acs
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.acs.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +18,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val url = "https://harshpatel63.github.io/Decentralized-Rating-System/"
-        val i = Intent(Intent.ACTION_VIEW)
-        i.setPackage("io.metamask")
-        i.data = Uri.parse(url)
-        startActivity(i)
+        if(isAppInstalled("io.metamask")) {
+            val url = "https://recondite-astonishing-radius.glitch.me/"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.setPackage("io.metamask")
+            i.data = Uri.parse(url)
+            startActivity(i)
+        } else {
+            Toast.makeText(this, "Please install metamask to continue...", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun isAppInstalled(packageName: String): Boolean {
+        return try {
+            packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (ignored: PackageManager.NameNotFoundException) {
+            false
+        }
     }
 }
