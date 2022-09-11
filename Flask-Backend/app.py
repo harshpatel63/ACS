fdb98230cf0d047ae076e4594384468645626904
thisisbipin <bipinmjadav087@gmail.com>
2022-09-11 18:29:49 +0530

    backend updated

---
+++ Flask-Backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn import preprocessing
+
+app = Flask(__name__)
+
+
+@app.route('/inputs/<uid1>/<uid2>/<uid3>', methods=['GET'])
+def index(uid1, uid2, uid3):
+    if request.method == 'GET':
+        model = pickle.load(open('_model.pkl', 'rb'))
+        d2 = preprocessing.normalize([[uid1, uid2, uid3]])
+        y_pred = model.predict(d2)
+        print(y_pred)
+        print(y_pred[0])
+        output = str(y_pred[0])
+        data = {
+            "confidence": output
+        }
+        return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
